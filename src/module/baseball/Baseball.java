46582c030ecfc88498ed570c1786b9b2c558d9c9
WNSGUD\ffjjo <ddd@ddd.com>
2024-06-28 16:17:56 +0900

    Feat : 숫자야구 게임 추가

---
+++ src/module/baseball/Baseball.java
@@ -0,0 +1,15 @@
+package module.baseball;
+
+import module.Playable;
+
+public class Baseball implements Playable {
+    @Override
+    public void init() {
+        
+    }
+
+    @Override
+    public void playGame() {
+
+    }
+}
